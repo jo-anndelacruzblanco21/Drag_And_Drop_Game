5f5db4e2bcc4aa7b0cf7df59345a9945b764d485
jo-anndelacruzblanco21 <151493574+jo-anndelacruzblanco21@users.noreply.github.com>
2026-05-20 20:40:27 +0800

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -1,38 +1,81 @@
+// indexScript.js
+
 const draggableElement = document.querySelectorAll('.box');
+
 const droppableElement = document.querySelectorAll('.droppable');
+
 const remarksDiv = document.getElementById('remarksDiv');
+
+const scoreSection = document.getElementById('scoreSection');
+
 let score = 0;
 
+// DRAG EVENTS
 draggableElement.forEach(element => {
+
     element.addEventListener('dragstart', (drgStrt) => {
+
         drgStrt.dataTransfer.setData('text', drgStrt.target.id);
+
         drgStrt.currentTarget.classList.add('draggableFormat');
+
     });
 
-    element.addEventListener('dragend', (drgendEvt) => {
-        drgendEvt.currentTarget.classList.remove('draggableFormat');
+    element.addEventListener('dragend', (drgEnd) => {
+
+        drgEnd.currentTarget.classList.remove('draggableFormat');
+
     });
+
 });
 
+// DROP EVENTS
 droppableElement.forEach(element => {
+
     element.addEventListener('dragover', (dragOver) => {
+
         dragOver.preventDefault();
+
     });
 
     element.addEventListener('drop', (dropEvt) => {
+
         dropEvt.preventDefault();
-        const droppedElementId = dropEvt.dataTransfer.getData('text');
-        const dropZoneId = dropEvt.target.getAttribute('data-draggable-id');
-        const droppedElement = document.getElementById(droppedElementId);
 
-        dropEvt.target.appendChild(droppedElement);
+        const droppedElementId =
+            dropEvt.dataTransfer.getData('text');
 
-        if (dropZoneId === droppedElementId.replace('drag', 'drop')) {
+        const droppedElement =
+            document.getElementById(droppedElementId);
+
+        const dropZoneId =
+            dropEvt.currentTarget.getAttribute('data-draggable-id');
+
+        // MOVE IMAGE
+        dropEvt.currentTarget.innerHTML = "";
+        dropEvt.currentTarget.appendChild(droppedElement);
+
+        // CHECK ANSWER
+        if(dropZoneId === droppedElementId.replace('drag', 'drop')){
+
             score++;
-            remarksDiv.innerText = 'Correct!';
-            scoreSection.innerText = `Score: ${score}`;
+
+            remarksDiv.innerText = "✅ Correct!";
+
+            dropEvt.currentTarget.style.backgroundColor =
+                "lightgreen";
+
         } else {
-            remarksDiv.innerText = 'Wrong!';
+
+            remarksDiv.innerText = "❌ Wrong!";
+
+            dropEvt.currentTarget.style.backgroundColor =
+                "#ffb3b3";
         }
+
+        // UPDATE SCORE
+        scoreSection.innerText = `Score: ${score}`;
+
     });
+
 });
